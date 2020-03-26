7a60f1ea5d279df9cea2d78b748547d6125c3e66
Ony <onyhoque@gmail.com>
2020-03-26 13:12:24 +0600

    temp 1

--- app/src/main/java/com/example/handler/Home.java
+++ app/src/main/java/com/example/handler/Home.java
@@ -3,22 +3,34 @@ package com.example.handler;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 public class Home extends AppCompatActivity {
 
     ImageView image1,image2,temp;
+    Bitmap img1,img2,bitTemp;
     Button btn_select;
 
 
@@ -34,22 +46,30 @@ public class Home extends AppCompatActivity {
         image1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openGallery(image1);
+                openGallery(image1,img1);
             }
         });
 
         image2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openGallery(image2);
+                openGallery(image2,img2);
+            }
+        });
+
+        btn_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                save_to_DB();
             }
         });
     }
 
     private static final int PICK_IMAGE = 100;
 
-    private void openGallery(ImageView imgViewer) {
+    private void openGallery(ImageView imgViewer, Bitmap bitmap) {
         temp = imgViewer;
+        bitTemp = bitmap;
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
     }
@@ -59,8 +79,37 @@ public class Home extends AppCompatActivity {
         if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
             Uri imageUri = data.getData();
             temp.setImageURI(imageUri);
+            try {
+                bitTemp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                save_to_DB(bitTemp);
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(),"error: "+e,Toast.LENGTH_LONG).show();
+            }
         }
     }
 
+    void save_to_DB(Bitmap bitmap){
+        SharedPreferences prefs = getSharedPreferences("SharedInfo", MODE_PRIVATE);
+        String dbName = prefs.getString("dbName", "");
+        String tableName = prefs.getString("tableName", "");
+        if(!(dbName.equals("") || tableName.equals(""))){
 
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,80,stream);
+            byte[] imageByte = stream.toByteArray();
+            String str = new String(imageByte);
+
+            try {
+                SQLiteDatabase mydatabase = openOrCreateDatabase(dbName, MODE_PRIVATE, null);
+                mydatabase.execSQL("INSERT INTO " + tableName + " VALUES('img1','"+ str + "');");
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(),"DB: "+e,Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+
+        }else{
+            Toast.makeText(getApplicationContext(),"No DB name or Table name!\ndbName: "+dbName+
+                    "\ntableName: "+tableName,Toast.LENGTH_LONG).show();
+        }
+    }
 }
--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -5,18 +5,25 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_enter, btn_reset;
     EditText edit_pin;
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +34,24 @@ public class MainActivity extends AppCompatActivity {
         btn_reset = findViewById(R.id.btn_reset);
         edit_pin = findViewById(R.id.edit_pin);
 
-        Req_Permission();
+
+        if(flag){
+            flag =false;
+            check_data();
+            Req_Permission();
+        }
+
 
         btn_enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String pin = edit_pin.getText().toString();
-                if(pin.equals("1234")){
-                    Intent togo = new Intent(getApplicationContext(), Home.class);
-                    startActivity(togo);
-                }
+//                if(pin.equals("1234")){
+//                    Intent togo = new Intent(getApplicationContext(), Home.class);
+//                    startActivity(togo);
+//                }
+                Intent togo = new Intent(getApplicationContext(), Home.class);
+                startActivity(togo);
 
             }
         });
@@ -46,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast toast=Toast.makeText(getApplicationContext(),"Not implemented yet.",Toast.LENGTH_SHORT);
                 toast.show();
+//                try{
+//                    check_data();
+//                }catch (Exception e){
+//                    Toast.makeText(getApplicationContext(),"Error: "+e,Toast.LENGTH_LONG).show();
+//                }
+
             }
         });
     }
@@ -56,6 +77,23 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     1);
+        }
+    }
+
+    void check_data(){
+        SharedPreferences prefs = getSharedPreferences("SharedInfo", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        int newStart = prefs.getInt("newStart", 0);
+        if(newStart == 0){
+            Toast.makeText(getApplicationContext(),"New start",Toast.LENGTH_SHORT).show();
+            editor.putInt("newStart", 1);
+            editor.putString("dbName","myDB");
+            editor.putString("tableName","table1");
+            SQLiteDatabase mydatabase = openOrCreateDatabase("myDB",MODE_PRIVATE,null);
+            mydatabase.execSQL("CREATE TABLE IF NOT EXISTS table1(name VARCHAR,image VARCHAR);");
+            editor.apply();
+        }else{
+            Toast.makeText(getApplicationContext(),"Already started",Toast.LENGTH_SHORT).show();
         }
     }
 }
